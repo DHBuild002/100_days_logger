46899d3d265febfa122b7c0d9c401c85e076c75c
Dan Haddock <dhbuild.002@gmail.com>
2020-09-27 11:54:28 +0100

    Overall BUG: Unable to persist data using NEDB -
    Current  BUG: Uncaught ReferenceError: logEntry is not defined on ln 29: 'database.insert(logEntry);'
    I have put logEntry into a seperate function in the app section of my index.js file.
    This new Class Event is now no longer triggering
    It is being called from handlers.logEntry() ln.44
    
    FIX: WIP

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import "./modal-styles.css";
 import "./modal.js";
 import { database } from '../../server/database.js';
+import Log from './log.js';
 
 const app = {
   addLogRow: (tableID) => {
@@ -13,7 +14,7 @@ const app = {
       let noteCell = newRow.insertCell(1);
       let urlCell = newRow.insertCell(2);
 
-      let addLogInput_day = document.getElementById("input-date").value;
+      let addLogInput_day = document.getElementById("input-day").value;
       let addLogInput_note = document.getElementById("input-log").value;
       let addLogInput_url = document.getElementById("input-url").value;
 
@@ -21,7 +22,13 @@ const app = {
       noteCell.innerHTML = addLogInput_note;
       urlCell.innerHTML = addLogInput_url;
 
-      newRow.className = 'log'
+      newRow.className = 'log';
+  },
+  newLog: () => {
+    let logEntry = new Log("1", addLogInput_note, addLogInput_url);
+      database.insert(logEntry);
+      console.log(logEntry);
+      
   },
   removeLogRow: (tableID) => {
     let tableRowRemove = document.getElementById("my-table").deleteRow(-1);
@@ -31,6 +38,10 @@ const app = {
 const handlers = {
     addLog: () => {
         app.addLogRow('#my-table');
+    },
+    newLog: () => {
+      console.log('New Log added to DB');
+      app.newLog();
     },
     removeLog: () => {
       let removeButtonClicked = true;
@@ -48,6 +59,7 @@ const view = {
     btn.addEventListener("click", function () {
       console.log("Click Detected");
       handlers.addLog();
+      handlers.newLog();
     });
   },
   removeLastItem: () => {
---
+++ client/src/log.js
@@ -0,0 +1,9 @@
+export default class Log {
+    constructor(day, note, link) {
+        this.day = day;
+        this.note = note;
+        this.link = link;
+    } 
+}
+
+ 
