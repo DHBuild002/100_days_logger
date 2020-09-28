57c584358e20840cc3629b8629c004a95e01cdbb
Dan Haddock <dhbuild.002@gmail.com>
2020-09-28 15:40:07 +0100

    Removed Autoload and went back to how I had nedb setup originally, loading DB on runtime of the localhost

--- server/database.js
+++ server/database.js
@@ -1,5 +1,5 @@
 const Datastore = require('nedb');
-const database = new Datastore({ filename: 'log.db', autoload: true })
+const database = new Datastore({ filename: 'log.db' });
 
 database.insert({testEntry: "Just a test from database.js"});
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const open = require("open");
 
 import webpack from 'webpack';
 import config from '../webpack.config.dev.js';
+import { database } from './database.js';
 
 const port = 8001;
 const app = express();
@@ -15,12 +16,13 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/src/index.html'));
+  res.sendFile(path.join(__dirname, '../client/src/index.html', './database.js'));
 });
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
     open('http://localhost:' + port);
+    database.loadDatabase();
   }
 });
