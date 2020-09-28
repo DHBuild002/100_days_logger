ffdd6deace3f26cbd73564575ddfe0e40d6785c6
Dan Haddock <dhbuild.002@gmail.com>
2020-09-28 15:28:30 +0100

    Remove two insert tests from database.js

--- server/database.js
+++ server/database.js
@@ -2,7 +2,5 @@ const Datastore = require('nedb');
 const database = new Datastore({ filename: 'log.db', autoload: true })
 
 database.insert({testEntry: "Just a test from database.js"});
-database.insert({testEntry: "Just a second test from database.js"});
-database.insert({testEntry: "Autoload test from database.js"});
 
 export { database };
